a3bedd74ae50e0352218f5f0bfb87d151f8bed0d
HendAdel1 <1hend.adel1@gmail.com>
2023-06-18 22:55:47 +0300

    categorycontroller created, controller views, bookcontroller to add need category thing

--- laravel/database/migrations/2023_06_17_154654_create_categories_table.php
+++ laravel/database/migrations/2023_06_17_154654_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description')->nullable();
+            $table->integer('num_books')->default(0);
             $table->timestamps();
         });
     }
