c02020f78d7418a2284bc49c9043e331620740a5
HendAdel1 <1hend.adel1@gmail.com>
2023-06-19 16:56:42 +0300

    edit api.php

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
 use App\Http\Controllers\API\CategoriesController;
-=======
 // use App\Models\User;
 
->>>>>>> 8330a41225a92d6b5faa102a46b12f24f35c710e
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,14 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 
 });
-<<<<<<< HEAD
 Route::get('/categories', [CategoriesController::class, 'index']);
 Route::get('/categories/{id}', [CategoriesController::class, 'show']);
 Route::post('/categories', [CategoriesController::class, 'store']);
 Route::put('/categories/{id}', [CategoriesController::class, 'update']);
 Route::delete('/categories/{id}', [CategoriesController::class, 'destroy']);
 
-=======
 Route::apiResource('users','App\Http\Controllers\API\UsersController');
 //register
 Route::post('create',[App\Http\Controllers\API\AuthController::class,'create']);
@@ -38,4 +33,3 @@ Route::post('create',[App\Http\Controllers\API\AuthController::class,'create']);
 Route::post('login',[App\Http\Controllers\API\AuthController::class,'login']);
 //update
 // Route::PUT(`/user/update/{$id}`,[App\Http\Controllers\API\UsersController::class,'update']);
->>>>>>> 8330a41225a92d6b5faa102a46b12f24f35c710e
