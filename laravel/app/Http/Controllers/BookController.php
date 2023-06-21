f6925ac4fcfc572804dd39c4bbfb022822bac52c
Doaa-Ghopashe <doaaadel252@yahoo.com>
2023-06-21 12:37:39 +0300

    some updates

--- laravel/app/Http/Controllers/BookController.php
+++ laravel/app/Http/Controllers/BookController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 // use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Models\Book;
+use App\Models\Category;
+
 
 class BookController extends Controller
 {
     //
     public function index(){
         //get all books and order them by their name and the last updated one
-        $books = DB::table('books')->select(['title','image','description','']);
+        $books = DB::table('books')->
+        join('book_category','book_category.book_id','=','books.id')
+        ->__select(['books.title','books.description','books.image','book_category'])
+        ->groupBy(['books.title','books.description','books.image','book_category'])->get();
+        // join('book_category','book_category.book_id','=','books.id')->
+        // select(['books.title','books.image','books.description'])->get();
         return $books;
     }
 
@@ -21,17 +29,17 @@ class BookController extends Controller
     }
 
     //
-    public function edit(){
+    public function edit($id){
         return "book edited";
     }
 
     //
-    public function delete(){
+    public function delete($id){
         return "book deleted";
     }
 
     //
-    public function show(){
+    public function show($id){
         //
     }
 
